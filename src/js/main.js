631dcee0f07bc4ac98780bd9f703a6f048c4b1e9
Buldakov <abuldakov10@gmail.com>
2023-04-26 15:56:06 +0400

    Add main slider

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,13 @@ $(document).on('DOMContentLoaded', () => {
   // Open menu
   $('.js-header-burger').on('click', function () {
     $(this).closest('.header__nav').addClass('open');
+    $('body').addClass('no-scroll');
   });
 
   // Close menu
   $('.js-menu-close').on('click', function () {
     $(this).closest('.header__nav').removeClass('open');
+    $('body').removeClass('no-scroll');
   });
 
   // Close menu on click out of menu
@@ -16,6 +18,25 @@ $(document).on('DOMContentLoaded', () => {
 
     if (!menu.is(e.target) && menu.has(e.target).length === 0) {
       $('.header__nav').removeClass('open');
+      $('body').removeClass('no-scroll');
     }
+  });
+
+  /*** Main slider ***/
+  new Swiper('.js-slider', {
+    speed: 800,
+    allowTouchMove: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  $('.js-btn-next').on('click', function () {
+    $('.main-slider__next').click();
+  });
+
+  $('.js-btn-prev').on('click', function () {
+    $('.main-slider__prev').click();
   });
 });
